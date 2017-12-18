bf36e0ff88d10c205d7127f851ea83061648b748
sa-nakamura <sa-nakamura@voltage@co.jp>
2017-12-18 16:39:40 +0900

    [vol codecheck] https://qiita.com/re-24/items/542f39220a606e319fa1 より模索

--- app/Main.php
+++ app/Main.php
@@ -3,6 +3,12 @@
 function run ($argc, $argv)
 {
   foreach ($argv as $index=>$value) {
-    printf("argv[%s]: %s\n", $index, $value);
-  }
+	$base_url = 'http://challenge-server.code-check.io/';
+
+	$response = file_get_contents(
+                  $base_url.'/api/hash/'
+            );
+	$result = json_decode($response,true);
+
+	  }
 }
